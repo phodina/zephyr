98ee61947a724013d6341ad431990efb79130556
Petr Hodina <phodina@protonmail.com>
2020-12-18 20:37:16 +0100

    boards: lpc17xx: Add BSP for lpc1768
    Add board definiton files for LPC1768 aka btt_skr_v1.3
    
    Signed-off-by: Petr Hodina <phodina@protonmail.com>

---
+++ soc/arm/nxp_lpc/lpc176x/soc.h
@@ -0,0 +1,24 @@
+ /*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief Board configuration macros for the nxp_lpc17x platform
+ *
+ * This header file is used to specify and describe board-level aspects for the
+ * 'nxp_lpc17x' platform.
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifndef _ASMLANGUAGE
+#include <sys/util.h>
+
+/* Add include for DTS generated information */
+#include <devicetree.h>
+
+#endif /* !_ASMLANGUAGE */
+
+#endif /* _SOC__H_ */
